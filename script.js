9b5bf314ad446c4772cc414164ac7240a35c8fb5
Nikita <jrringler13759@gmail.com>
2020-04-12 13:59:24 -0400

    rethinking the functions and ajax calls

--- script.js
+++ script.js
@@ -1,51 +1,97 @@
 
-
-// api.openweathermap.org/data/2.5/weather?q={city name}&APPID=c69b1eeef58f9f4426fef5e9f3e9f49c
-
-//when the start button is clicked
-//the city is listed below and the weather appears on the right.
-
     
 var cities = [];
-console.log(cities);
 
-function displayWeatherInfo() {
-    var city = $("#cityInput").val().trim();
-   
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=62fca606199df2afea0a32e25faffdc5";
-    console.log(queryURL);
-    $.ajax({
-        url:queryURL,
-        
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-        var temp = response.main.temp;
+
+//function updateCurrent(cityInfo) {
     
-        $("#temp").text(temp);
-        $("#tempDayOne").text(temp);
-    });
-}
+    //var mainTemp = $("#temp").text(cityInfo.main.temp);
+
+
+   
+//}
+
+//function updateFiveDay(forecast){
+//}
+
+//function uvIndex(lat, long){
+//}
+
+
 
 function renderSearchHistory() {
     $("#searchLog").empty();
     for (var i = 0; i < cities.length; i++){
-        var newSearch = $("<button>");
-        newSearch.attr("data-name", cities[i]);
-        newSearch.text(cities[i]);
-        $("#searchLog").prepend(newSearch);
+        displayNewCity(cities[i]);
     }
-    
-}   
- 
+} 
+
+function displayNewCity (city){
+    var newSearch = $("<button class = 'cityBtn'>");
+    newSearch.attr("data-name", city);
+    newSearch.text(city);
+    $("#searchLog").prepend(newSearch);   
+}
+
+
+//LOCAL STORAGE
+//function onLoad() {
+//localStorage.getItem("city")
+//}
+
+//is it ok to have two click event on the same button?
 $("#searchBtn").on("click", function(event){
     event.preventDefault();
     var city = $("#cityInput").val().trim();
-    cities.push(city);
-    renderSearchHistory();
-    displayWeatherInfo();
+    console.log(city);
+    
+    if (city) {
+        if (cities.indexOf(city)=== -1){
+            cities.push(city);
+            renderSearchHistory(city);
+            localStorage.setItem("city", city);
+        }
+        //updateCurrent();
+        $("#cityInput").val("");
+    }
+    
 })   
+
+$("#searchBtn").on("click", function(event){
+    event.preventDefault();
+   
+    $("#weather").show();
+    var city = $("#cityInput").val().trim();
     
-displayWeatherInfo();
-renderSearchHistory();   
-    
+    //not showing up
+    console.log(city);
+   
+    var currentQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=62fca606199df2afea0a32e25faffdc5";
+$.ajax({
+    url: currentQueryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+})
+});
+
+
+
+/*
+    var forecastQueryURL= "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=62fca606199df2afea0a32e25faffdc5";;
+$.ajax({
+    url:forecastQueryURL,
+    method: "GET"
+}).then(updateForecast)
+
+
+
+    var uvIndexQueryURL;
+$.ajax({
+    url:uvIndexQueryURL,
+    method: "GET"
+}).then(uvIndex)
+*/
+
+
+$("#weather").hide();
